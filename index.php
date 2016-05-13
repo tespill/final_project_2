390cb1e645477e583544275f714c5f20f5b1a12a
Aiden Navarro <aiden.navarro@west-mec.org>
2016-05-12 17:59:14 -0700

    no message

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once('structure/header.php');
 ?>
 
 <body>
+
 <!-- START OF NAVIGATION BAR -->
 <div id="navigation">
     <div id="sidebar-wrapper">
@@ -38,95 +39,18 @@ require_once('structure/header.php');
 <!---  END OF NAVIGATION BAR -->
 
 <div id="main" class="container text-center">
-    <div class="row">
-        <div class="col-sm-7">
-
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="panel panel-default text-left">
-                        <div class="panel-body">
-                            <p contenteditable="true">Status: Feeling Blue</p>
-                            <button type="button" class="btn btn-default btn-sm">
-                                <span class="glyphicon glyphicon-thumbs-up"></span> Like
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="well">
-                        <p>John</p>
-                        <img src="bird.jpg" class="img-circle" height="55" width="55" alt="Avatar">
-                    </div>
-                </div>
-                <div class="col-sm-9">
-                    <div class="well">
-                        <p>Just Forgot that I had to mention something about someone to someone about how I forgot
-                            something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="well">
-                        <p>Bo</p>
-                        <img src="bandmember.jpg" class="img-circle" height="55" width="55" alt="Avatar">
-                    </div>
-                </div>
-                <div class="col-sm-9">
-                    <div class="well">
-                        <p>Just Forgot that I had to mention something about someone to someone about how I forgot
-                            something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="well">
-                        <p>Jane</p>
-                        <img src="bandmember.jpg" class="img-circle" height="55" width="55" alt="Avatar">
-                    </div>
-                </div>
-                <div class="col-sm-9">
-                    <div class="well">
-                        <p>Just Forgot that I had to mention something about someone to someone about how I forgot
-                            something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="well">
-                        <p>Anja</p>
-                        <img src="bird.jpg" class="img-circle" height="55" width="55" alt="Avatar">
-                    </div>
-                </div>
-                <div class="col-sm-9">
-                    <div class="well">
-                        <p>Just Forgot that I had to mention something about someone to someone about how I forgot
-                            something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="right" class="col-sm-2 well">
-            <div class="thumbnail">
-                <p>Upcoming Events:</p>
-                <img src="paris.jpg" alt="Paris" width="400" height="300">
-                <p><strong>Paris</strong></p>
-                <p>Fri. 27 November 2015</p>
-                <button class="btn btn-primary">Info</button>
-            </div>
-            <div class="well">
-                <p>ADS</p>
-            </div>
-            <div class="well">
-                <p>ADS</p>
-            </div>
-        </div>
+    <!-- JQuery Slider [responsiveslides.com] -->
+    <div id="slider">
+        <ul class="rslides">
+            <li><img src="slider1.jpg" alt="slider1"></li>
+            <li><img src="slider2.jpg" alt="slider2"></li>
+            <li><img src="slider3.jpg" alt="slider3"></li>
+        </ul>
     </div>
+    <br/><hr/><br/>
+    <!-- End of slider -->
+
+
 </div>
 
 <footer class="container-fluid text-center">
@@ -140,6 +64,13 @@ require_once('structure/header.php');
 
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
+
+<!-- Slider -->
+<script>
+    $(function() {
+        $(".rslides").responsiveSlides(); // Starts slider
+    });
+</script>
 
 </body>
 </html>
--- structure/header.php
+++ structure/header.php
@@ -29,4 +29,10 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!--Slider-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="js/responsiveslides.js"></script>
+    <script src="js/responsiveslides.min.js"></script>
+
 </head>
